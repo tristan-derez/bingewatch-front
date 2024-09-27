5b1a8d4edd512b670f0095f050d4f918cfa25f62
tristan-derez <tristan.derez@gmail.com>
2024-09-27 14:32:22 +0200

    Add lucide-react and nav buttons
    - Nav icons now turns red on hover and are white when the active page correspond to their title, eg: user is on page movies, movie icon is white
    - some small tweaks on padding depending on screen size
    - An user icon is now shown instead of profile picture when user is not logged in

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,26 @@
-import { Flex, Box } from "#styled-system/jsx";
+import { Flex, Box, styled } from "#styled-system/jsx";
+import { useState } from "react";
 import { HomeIcon } from "./nav-icons/home-icon";
 import { LogoIcon } from "./nav-icons/logo-icon";
 import { NavBookMarkIcon } from "./nav-icons/nav-bookmark-icon";
 import { NavMovieIcon } from "./nav-icons/nav-movie-icon";
 import { NavTvSerieIcon } from "./nav-icons/nav-tv-series-icon";
+import { CircleUser } from "lucide-react";
+
+type PageName = "home" | "movies" | "tvSeries" | "bookmarks";
 
 function NavBar() {
+  const [activePage, setActivePage] = useState<PageName>("home");
+
+  const handleNavClick = (page: PageName) => {
+    setActivePage(page);
+    console.log(`${page} clicked`);
+  };
+
+  const getIconColor = (page: PageName) => {
+    return activePage === page ? "white" : "gray";
+  };
+
   return (
     <Box
       m={{ base: 0, md: 0, lg: "32px" }}
@@ -19,11 +34,10 @@ function NavBar() {
         justifyContent={{ base: "space-between", lg: "flex-start" }}
         alignItems='center'
         bg='surface'
-        p='16px'
         h={{ base: "64px", lg: `calc(100vh - 64px)` }}
         borderRadius='20px'
       >
-        <Box mb={{ lg: "75px" }}>
+        <Box px={{ base: "16px", md: "24px", lg: 0 }} pt={{ lg: "36px" }} pb={{ lg: "75px" }}>
           <LogoIcon />
         </Box>
 
@@ -32,30 +46,62 @@ function NavBar() {
           justify={{ base: "center", lg: "flex-start" }}
           gap={{ base: "24px", md: "32px", lg: "40px" }}
         >
-          <Box>
-            <HomeIcon />
-          </Box>
-          <Box>
-            <NavMovieIcon />
-          </Box>
-          <Box>
-            <NavTvSerieIcon />
-          </Box>
-          <Box>
-            <NavBookMarkIcon />
-          </Box>
+          <NavButton onClick={() => handleNavClick("home")}>
+            <HomeIcon color={getIconColor("home")} />
+          </NavButton>
+          <NavButton onClick={() => handleNavClick("movies")}>
+            <NavMovieIcon color={getIconColor("movies")} />
+          </NavButton>
+          <NavButton onClick={() => handleNavClick("tvSeries")}>
+            <NavTvSerieIcon color={getIconColor("tvSeries")} />
+          </NavButton>
+          <NavButton onClick={() => handleNavClick("bookmarks")}>
+            <NavBookMarkIcon color={getIconColor("bookmarks")} />
+          </NavButton>
         </Flex>
 
-        <Flex alignSelf={{ base: "flex-end", lg: "center" }} mt={{ base: "auto" }} mb={{ lg: "0" }}>
-          <img
-            src='/src/assets/image-avatar.png'
-            alt='Profile'
-            style={{ borderRadius: "50%", width: "32px", height: "32px" }} // Adjust width and height as needed
-          />
+        <Flex
+          alignSelf={{ base: "flex-end", lg: "center" }}
+          mt={{ base: "auto" }}
+          mb={{ lg: "0" }}
+          py={{ base: "16px", md: "16px", lg: "32px" }}
+          px={{ base: "16px", md: "19px", lg: 0 }}
+        >
+          <UserButton>
+            <CircleUser size={32} color='gray' />
+          </UserButton>
         </Flex>
       </Flex>
     </Box>
   );
 }
+
+const NavButton = styled("button", {
+  base: {
+    bg: "transparent",
+    border: "none",
+    cursor: "pointer",
+    p: "8px",
+    borderRadius: "8px",
+    "&:hover": {
+      "& svg:not(.circle-user) path": {
+        fill: "red",
+        transition: "fill 0.6s ease",
+      },
+    },
+    "&:focus": {
+      outline: "none",
+      boxShadow: "0 0 0 2px secondary",
+    },
+  },
+});
+
+const UserButton = styled("button", {
+  base: {
+    bg: "transparent",
+    border: "none",
+    cursor: "pointer",
+  },
+});
 
 export default NavBar;
