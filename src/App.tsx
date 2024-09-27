8ebe417a9230385dc2862e0fc48554ea435824df
tristan-derez <tristan.derez@gmail.com>
2024-09-27 16:59:03 +0200

    change div with role "main"

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,9 @@ function App() {
   const [isSearchActive, setIsSearchActive] = useState(false);
 
   return (
-    <Flex
-      w='100%'
-      maxW='100vw'
-      minH='100vh'
-      color='text'
-      bg='background'
-      flexDirection={{ base: "column", lg: "row" }}
-      role='main'
-    >
+    <Flex w='100%' maxW='100vw' minH='100vh' color='text' bg='background' flexDirection={{ base: "column", lg: "row" }}>
       <NavBar />
-      <Flex flexDir='column' w='100%' pl={{ lg: "146px" }} pb={{ lg: "24px" }}>
+      <Flex flexDir='column' w='100%' pl={{ lg: "146px" }} pb={{ lg: "24px" }} role='main'>
         <MovieSearch movies={movies} onSearchActiveChange={setIsSearchActive} />
         {!isSearchActive && (
           <>
