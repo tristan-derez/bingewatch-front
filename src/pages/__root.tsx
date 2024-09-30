122a70fbd6885846984109bf0ae3171f7ba3f51b
tristan-derez <tristan.derez@gmail.com>
2024-09-30 14:22:44 +0200

    rm rules for wider screens

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,7 +1,6 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { Flex } from "#styled-system/jsx";
 import NavBar from "#components/navbar";
-import { css } from "#styled-system/css/";
 
 export const Route = createRootRoute({
   component: Root,
@@ -9,31 +8,7 @@ export const Route = createRootRoute({
 
 export default function Root() {
   return (
-    <Flex
-      w='100%'
-      maxW='100vw'
-      minH='100vh'
-      color='text'
-      bg='background'
-      flexDirection={{ base: "column", lg: "row" }}
-      className={css({
-        "@media (min-width: 2000px)": {
-          maxWidth: "2000px",
-          margin: "0 auto",
-          position: "relative",
-          "&::before": {
-            content: '""',
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "var(--colors-background)",
-            zIndex: -1,
-          },
-        },
-      })}
-    >
+    <Flex w='100%' maxW='100vw' minH='100vh' color='text' bg='background' flexDirection={{ base: "column", lg: "row" }}>
       <NavBar />
       <Flex flexDir='column' w='100%' pl={{ lg: "164px" }} pb={{ lg: "24px" }} role='main'>
         <Outlet />
